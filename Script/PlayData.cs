3b7e6933c08e25574dcb700b5f134b73f92b8f42
Nullsuke <nullsuke@gmail.com>
2020-10-25 22:33:46 +0900

    スコア、レベル、削除したラインの表示

---
+++ Script/PlayData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayData : MonoBehaviour
+{
+    [SerializeField] Text ScoreText = default;
+    [SerializeField] Text LevelText = default;
+    [SerializeField] Text LineText = default;
+
+    public void  Render(int score,int level,int deletedLines)
+    {
+        ScoreText.text = score.ToString();
+        LevelText.text = level.ToString();
+        LineText.text = deletedLines.ToString();
+    }
+}
