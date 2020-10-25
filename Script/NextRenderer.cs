1aeeca430a1fc51fe8b39137ffb277b6f49e77d2
Nullsuke <nullsuke@gmail.com>
2020-10-25 22:32:50 +0900

    ネクスト表示関係

---
+++ Script/NextRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextRenderer : MonoBehaviour
+{
+    [SerializeField] Image[] nextImages = default;
+    [SerializeField] Sprite[] tetriminoSprites = default;
+
+    public void Render(Queue<int> queue)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int n = queue.ToArray()[i];
+            nextImages[i].sprite = tetriminoSprites[n];
+        }
+    }
+}
