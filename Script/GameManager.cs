0cba230e37e7cfd9f367c452b65bcc91b5441337
Nullsuke <nullsuke@gmail.com>
2020-10-25 22:28:59 +0900

    スコアの更新、クリア画面への遷移など

---
+++ Script/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    [SerializeField] TetriminoCreator tetriminoCreator = default;
+    [SerializeField] PlayData playData = default;
+    [SerializeField] GameObject gameOverPanel = default;
+    [SerializeField] GameObject clearPanel = default;
+
+    public void RenderPlayData(int score, int level, int deletedLines)
+    {
+        playData.Render(score, level, deletedLines);
+    }
+
+    public void Over()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Clear()
+    {
+        clearPanel.SetActive(true);
+    }
+
+    public void ReturnTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        tetriminoCreator.NewTetrimino(true);
+    }
+}
